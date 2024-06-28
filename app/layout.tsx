4207dbf35061dd3025533008cebd8f7f42a55a96
Guilherme Batista Bastos <guilherme.batistabastos@segment.com>
2024-06-28 00:57:00 -0700

    header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,12 @@ import './globals.css'
 export const metadata = {
   title: 'NextJS Course App',
   description: 'Your first NextJS app!',
-};
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
-  );
+  )
 }
