d435ae515249c3aa6f292447ca5ceb2a15e9d034
Guilherme Batista Bastos <guilherme.batistabastos@segment.com>
2024-09-17 23:41:07 -0700

    fix: content

--- app/about/hero.tsx
+++ app/about/hero.tsx
@@ -8,17 +8,24 @@ const Hero = () => {
     <div className={wrapper}>
       <div className={hero}>
         <div className={content}>
-          <h1>About</h1>
+          <h1>Hi, I'm Gui.</h1>
           <p>
-            Hi, I'm Gui. I'm about to start Game Development continuing studies
-            at Emily Carr. I hold a diploma in Software Engineering from both
-            PUC and Mackenzie. I have over 3 years of experience as a Senior
-            Software Engineer at Segment, and much more experience working for
-            various other companies. However, my passion has always been game
-            development, and I'm excited to embark on this new journey. I enjoy
-            working on diverse projects as they push me to explore new
-            technologies, collaborate with different teams, and continually grow
-            and learn.
+            I'm about to start Game Development continuing studies at Emily
+            Carr.
+          </p>
+          <p>
+            {' '}
+            I hold a diploma in Software Engineering from both PUC and
+            Mackenzie. I have over 3 years of experience as a Senior Software
+            Engineer at Segment, and much more experience working for various
+            other companies.
+          </p>
+          <p>
+            {' '}
+            However, my passion has always been game development, and I'm
+            excited to embark on this new journey. I enjoy working on diverse
+            projects as they push me to explore new technologies, collaborate
+            with different teams, and continually grow and learn.
           </p>
         </div>
         <div className={avatar}>
