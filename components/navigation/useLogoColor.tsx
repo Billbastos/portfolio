b68658d0203cda58e5401d9434db3133d05fbf84
Guilherme Bastos <gui.b.bastos@gmail.com>
2024-10-29 10:36:34 -0700

    fix: add animation hover logo link

--- components/navigation/useLogoColor.tsx
+++ components/navigation/useLogoColor.tsx
@@ -5,13 +5,13 @@ const useLogoColor = () => {
   const pathname = usePathname()
 
   useEffect(() => {
-    let color = 'var(--contact-bg-color-60)'
+    let color = 'var(--contact-bg-color-70)'
     if (pathname.includes('/about')) {
-      color = 'var(--about-bg-color-60)'
+      color = 'var(--about-bg-color-70)'
     } else if (pathname.includes('/game')) {
-      color = 'var(--game-bg-color-60)'
+      color = 'var(--game-bg-color-70)'
     } else if (pathname.includes('/blog')) {
-      color = 'var(--blog-bg-color-60)'
+      color = 'var(--blog-bg-color-70)'
     }
 
     document.documentElement.style.setProperty('--nav-custom-color', color)
